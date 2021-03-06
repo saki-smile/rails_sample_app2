7b06c74d3952d03c033768e4d01700e4c05fd852
saki-smile <sa.su.ko0419@gmail.com>
2021-03-06 01:12:50 +0000

    finish

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = 'b03e00cb405adbe35d30ee5e06c1104de217d8477f39e8746f83377b79331363155b3b7d6b58f50565a3c42257178f5b6b00ca70668bd6d29e831862d4e4e2c4'
--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,7 @@ Rails.application.routes.draw do
   post 'todolists' => 'todolist#create'
   get 'todolists' => 'todolist#index'
   get 'todolists/:id' => 'todolist#show', as: 'todolist'
+  get 'todolists/:id/edit' => 'todolist#edit', as: 'edit_todolist'
+  patch 'todolists/:id' => 'todolist#update', as: 'update_todolist'
+  delete 'todolists/:id' => 'todolist#destroy', as: 'destroy_todolist'
 end
